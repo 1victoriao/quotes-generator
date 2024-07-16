53c75434464380689e99af625cdf4db34d31d8f2
1victoriao <victoria.ona.anderson@gmail.com>
2024-07-16 15:28:34 -0500

    added typewriter feature to javascript and html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 function generateQuote(event) {
   event.preventDefault();
 
-  let quoteElement = document.querySelector("#quote");
-  quoteElement.innerHTML = "Quote will go here";
+  new Typewriter("#quotes", {
+    strings: "Special quote here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
 
 let quoteFormElement = document.querySelector("#quote-generator-form");
